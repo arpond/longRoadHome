378da36ee82f68e0380f350f914898e534149da0
Andrew Pond <a.r.pond@dundee.ac.uk>
2015-04-10 14:08:49 +0100

    Stated Work on LocationModel

--- LongRoadHome/LongRoadHome/Model/Location/LocationModel.cs
+++ LongRoadHome/LongRoadHome/Model/Location/LocationModel.cs
@@ -6,33 +6,97 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
     public class LocationModel
     {
         private List<Location> visitedLocation;
-        private List<DummyLocation> unvistedLocation;
+        private List<DummyLocation> unvisitedLocation;
         private Location currentLocation;
         private Sublocation currentSublocation;
 
-        public LocationModel(ref String visitedLocs, ref String unvisitedLocs, ref String currLoc, ref String currSLoc)
+        public LocationModel(String visitedLocs, String unvisitedLocs, String currLoc, String currSLoc)
         {
-            throw new System.Exception("Not implemented");
+            visitedLocation = new List<Location>();
+            unvisitedLocation = new List<DummyLocation>();
+
+            String[] visitedElem = visitedLocs.Split('#');
+            foreach(String loc in visitedElem)
+            {
+                Location temp = new Location(loc);
+                visitedLocation.Add(temp);
+            }
+
+            String[] unvisitedElem = unvisitedLocs.Split('#');
+            foreach(String loc in unvisitedElem)
+            {
+                DummyLocation temp = new DummyLocation(loc);
+                unvisitedLocation.Add(temp);
+            }
+
+            int currID;
+            if(int.TryParse(currLoc, out currID))
+            {
+                foreach (Location loc in visitedLocation)
+                {
+                    if (loc.GetLocationID() == currID)
+                    {
+                        currentLocation = loc;
+                        break;
+                    }
+                }
+            }
+
+            int currSub;
+            if (int.TryParse(currSLoc, out currSub))
+            {
+                currentLocation.GetSublocationByID(currSub);
+            }
         }
-        public String ParseToString()
+
+        public String ParseVisitedToString()
         {
-            throw new System.Exception("Not implemented");
+            String parsed = "VisitedLocations";
+            foreach(Location visited in visitedLocation)
+            {
+                parsed += "#" + visited.ParseToString();
+            }
+            return parsed;
         }
-        public Location ReplaceDummyLocation(ref DummyLocation dummy)
+
+        public String ParseUnvisitedToString()
         {
-            throw new System.Exception("Not implemented");
+            String parsed = "UnvisitedLocations";
+            foreach (DummyLocation unvisited in unvisitedLocation)
+            {
+                parsed += "#" + unvisited.ParseToString();
+            }
+            return parsed;
         }
+
+        public String ParseCurrLocationToString()
+        {
+            return "" + currentLocation.GetLocationID();
+        }
+
+        public String ParseCurrSubLocToString()
+        {
+            return "" + currentSublocation.GetSublocationID();
+        }
+
+        public Location ReplaceDummyLocation(DummyLocation dummy, int minSize, int maxSize, int maxItems, int maxAmount)
+        {
+            Location location = Location.ConvertToLocation(dummy);
+            location.GenerateSubLocations(minSize, maxSize, maxItems, maxAmount);
+            return location;
+        }
+
         public void GenerateDummyLocations(ref int maxBranchFactor)
         {
             throw new System.Exception("Not implemented");
         }
         public Sublocation GetSubLocation()
         {
-            throw new System.Exception("Not implemented");
+            return this.currentSublocation;
         }
         public String GetSublocType()
         {
-            throw new System.Exception("Not implemented");
+            return currentSublocation.GetType().ToString();
         }
         public void ChangeSubLocation(ref int subLocationID)
         {
@@ -48,13 +112,7 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
         }
         public bool IsScavenged()
         {
-            throw new System.Exception("Not implemented");
+            return currentSublocation.GetScavenged();
         }
-
-        private DummyLocation dummyLocation;
-        private Sublocation sublocation;
-
-        private uk.ac.dundee.arpond.longRoadHome.Model.GameState gameState;
-
     }
 }
---
+++ UnitTests-LongRoadHome/UnitTests-LongRoadHome/LocationTests/TLocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests_LongRoadHome.LocationTests
+{
+    [TestClass]
+    public class TLocationModel
+    {
+        [TestCategory("Location"), TestCategory("LocationModel"), TestMethod()]
+        public void LocationModel_StandardConstructor()
+        {
+        }
+    }
+}
