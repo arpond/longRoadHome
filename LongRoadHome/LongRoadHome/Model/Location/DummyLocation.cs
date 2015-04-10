58dfc38e3dd604ca6ec86c9275b9f4aaeaaf2e79
Andrew Pond <a.r.pond@dundee.ac.uk>
2015-04-10 09:34:52 +0100

    Completed and Tested Dummy Location

--- LongRoadHome/LongRoadHome/Model/Location/DummyLocation.cs
+++ LongRoadHome/LongRoadHome/Model/Location/DummyLocation.cs
@@ -4,15 +4,24 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
 {
     public class DummyLocation
     {
-        private HashSet<int> connections = new HashSet<int>();
+        protected HashSet<int> connections = new HashSet<int>();
         private int locationID;
         private String tag = "DummyLocation";
 
         public DummyLocation()
         {
-
+            locationID = 0;
         }
 
+        public DummyLocation(int locationID)
+        {
+            this.locationID = locationID;
+        }
+
+        /// <summary>
+        /// Parses DummyLocation from a string
+        /// </summary>
+        /// <param name="toParse">The string to parse from</param>
         public DummyLocation(String toParse)
         {
             String[] dlElems = toParse.Split(',');
@@ -38,11 +47,20 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
             }
         }
 
+        /// <summary>
+        /// Checks if two dummy locations are connected
+        /// </summary>
+        /// <param name="dl">The location to check if it is connected to</param>
+        /// <returns>If they are connected</returns>
         public bool IsConnected(DummyLocation dl)
         {
             return connections.Contains(dl.GetLocationID());
         }
 
+        /// <summary>
+        /// Gets the connections of a Dummylocation
+        /// </summary>
+        /// <returns>The connections</returns>
         public HashSet<int> GetConnections()
         {
             return this.connections;
@@ -52,14 +70,24 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
             this.connections = connections;
         }
 
+        /// <summary>
+        /// Gets the location ID
+        /// </summary>
+        /// <returns>The location ID</returns>
         public int GetLocationID()
         {
             return locationID;
         }
 
+        /// <summary>
+        /// Checks if the string is a valid dummy location
+        /// </summary>
+        /// <param name="toTest">The string to test</param>
+        /// <returns>Bool if it is valid</returns>
         public virtual bool IsValidDummyLocation(String toTest)
         {
-            int id;
+            HashSet<int> tempID = new HashSet<int>();
+            int id = -1;
             String[] dlElems = toTest.Split(',');
             if (dlElems.Length != 3)
             {
@@ -77,7 +105,7 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
                         }
                         break;
                     case "ID":
-                        if (locElem.Length != 2 || !int.TryParse(locElem[1], out id))
+                        if (locElem.Length != 2 || !int.TryParse(locElem[1], out id) || id <= 0)
                         {
                             return false;
                         }
@@ -87,7 +115,15 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
                         for (int i = 1; i < locElem.Length; i++)
                         {
                             int loc;
-                            if (!int.TryParse(locElem[i], out loc))
+                            if (int.TryParse(locElem[i], out loc))
+                            {
+                                if (tempID.Contains(loc) || loc == id || loc <= 0)
+                                {
+                                    return false;
+                                }
+                                tempID.Add(loc);
+                            }
+                            else
                             {
                                 return false;
                             }
@@ -100,12 +136,20 @@ namespace uk.ac.dundee.arpond.longRoadHome.Model.Location
             return true;
         }
 
+        /// <summary>
+        /// Parses the dummy location to a string
+        /// </summary>
+        /// <returns>the string representing the dummy location</returns>
         public virtual String ParseToString()
         {
             return String.Format("Type:{0},ID:{1},Connections{2}",tag, locationID, ParseConnections());
         }
         
-        protected String ParseConnections()
+        /// <summary>
+        /// Parses the connections
+        /// </summary>
+        /// <returns>String representing the parsed connections</returns>
+        private String ParseConnections()
         {
             String parse = "";
             foreach(int connection in connections)
---
+++ UnitTests-LongRoadHome/UnitTests-LongRoadHome/LocationTests/TLocation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using uk.ac.dundee.arpond.longRoadHome.Model.Location;
+using System.Collections.Generic;
+
+namespace UnitTests_LongRoadHome.LocationTests
+{
+    [TestClass]
+    public class TLocation
+    {
+        DummyLocation dl;
+        List<Tuple<String, String>> valid = new List<Tuple<String, String>>();
+        List<Tuple<String, String>> invalid = new List<Tuple<String, String>>();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            valid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1,Connections:2:3", "Standard dummy location is valid"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1","Should be at least 3 components"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1,Connections,Connections","At most 3 components"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,blah:1,Connections", "Components should be valid"));
+            invalid.Add(new Tuple<String, String>("Type:sadas,ID:,Connections,", "Type should be DummyLocaiton"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:sadas,Connections", "ID should be an int"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:-1,Connections", "ID should be positive"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1,Connections:", "If there are connections there should be at least one"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1,Connections:1", "Can't connect to self"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1,Connections:2:2", "Can't connect to locaiton more than once"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1,Connections:2:3:4:-1", "Connections should all be positive"));
+            invalid.Add(new Tuple<String, String>("Type:DummyLocation,ID:1,Connections:4:2::3", "Each should have a value"));
+        }
+
+        [TestCategory("Location"), TestCategory("DummyLocation"), TestMethod()]
+        public void DummyLocation_ParseToString()
+        {
+            String expected = "Type:DummyLocation,ID:1,Connections:2:3";
+            dl = new DummyLocation(expected);
+            Assert.AreEqual(expected, dl.ParseToString(), "String should be parsed correctly");
+        }
+
+        [TestCategory("Location"), TestCategory("DummyLocation"), TestMethod()]
+        public void DummyLocation_CheckStringIsValid()
+        {
+            dl = new DummyLocation();
+            foreach(Tuple<String,String> test in valid)
+            {
+                Assert.IsTrue(dl.IsValidDummyLocation(test.Item1),test.Item2);
+            }
+
+            foreach (Tuple<String,String> test in invalid)
+            {
+                Assert.IsFalse(dl.IsValidDummyLocation(test.Item1), test.Item2);
+            }
+            
+        }
+    }
+}
