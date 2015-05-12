efcb7a67891a8ce1392db4176754402cdcd09774
Andrew Pond <a.r.pond@dundee.ac.uk>
2015-05-12 11:08:08 +0100

    Wrote EventEffect created Test Class

---
+++ UnitTests-LongRoadHome/UnitTests-LongRoadHome/EventTests/TEventEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests_LongRoadHome.EventTests
+{
+    [TestClass]
+    public class TEventEffect
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+
+        [TestCategory("PREventEffect"), TestCategory("EventModel"), TestMethod()]
+        public void PREventEffect()
+        {
+
+        }
+    }
+}
